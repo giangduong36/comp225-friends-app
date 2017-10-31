c4f664558412e7582ddbc0610b4b44214054942d
bjelke <bjelke@macalester.edu>
2017-10-31 17:44:03 -0500

    Added slider. Not yet tested for android

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,8 @@ const {
     TouchableHighlight,
     AlertIOS,
     Button,
-    TextInput
+    TextInput,
+    Switch
 } = ReactNative;
 
 
@@ -26,72 +27,82 @@ const firebaseApp = require('../services/firebaseInit');
 
 class HomeScreen extends Component {
     static navigationOptions = {
-        title: 'Home',
+    title: 'Home',
     };
-
+    
     constructor(props) {
         super(props);
-		this.state = {
-			value: 0,
-		}
-
+        this.state = {
+        value: false,
+        }
+        
     }
-
+    
     render() {
         const {navigate} = this.props.navigation;
         return (
-            <View style={styles.container}>
-
+                <View style={styles.container}>
+                
                 {/*<Header text="home" loaded={this.state.loaded} />*/}
-
+                
                 <Text style={styles.welcome}>welcome to the home screen</Text>
-				<View style = {styles.buttonContainer}>
-				<ActionButton
-                    title="Profile"
-                    onPress={() => navigate('Profile')
-					}
-                />
-				</View>
-				<View style = {styles.buttonContainer}>
+                <View style = {styles.buttonContainer}>
                 <ActionButton
-                    title="Settings"
-                    onPress={() => navigate('Settings')
-					}
+                title="Profile"
+                onPress={() => navigate('Profile')
+                }
                 />
-				</View>
-				<Slider
-				value={this.state.value}
-				minimumValue={0}
-				maximumValue={1}
-				step={1}
-				minimumTrackTintColor='#1073ff'
-				maximumTrackTintColor='#b7b7b7'
-				style={sliderStyle.container}
-				trackStyle={sliderStyle.track}
-				thumbStyle={sliderStyle.thumb}
-				onValueChange={(value) => this.setState({value})} />
-				<Text>Value: {this.state.value}</Text>
-            </View>
-        );
+                </View>
+                <View style = {styles.buttonContainer}>
+                <ActionButton
+                title="Settings"
+                onPress={() => navigate('Settings')
+                }
+                />
+                </View>
+                
+                <Slider
+                //value={this.state.value}
+                minimumValue={0}
+                maximumValue={1}
+                step={1}
+                minimumTrackTintColor='#1073ff'
+                maximumTrackTintColor='#b7b7b7'
+                style={sliderStyle.container}
+                trackStyle={sliderStyle.track}
+                thumbStyle={sliderStyle.thumb}
+                onValueChange={(value) => this.setState({value})} />
+                
+                <Switch //toggle switch for availability
+                value = {this.state.value}
+                style={{marginBottom: 10}}
+                onValueChange={(value) => this.setState({value})}
+                />
+                //TODO: get rid of slider, improve switch style, pass data to firebase
+                
+                // <Text>Value: {this.state.value}</Text>
+                </View>
+                );
     }
-
+    
 }
 
 
 //https://snack.expo.io/H1cnedhBW
 var sliderStyle = StyleSheet.create({
-  track: {
-    height: 4,
-    borderRadius: 2,
-  },
-  thumb: {
-    width: 30,
-    height: 30,
-    borderRadius: 30 / 2,
-    backgroundColor: 'white',
-    borderColor: '#b7b7b7',
-    borderWidth: 2,
-  }
-});
+                                    track: {
+                                    height: 4,
+                                    borderRadius: 2,
+                                    },
+                                    thumb: {
+                                    width: 30,
+                                    height: 30,
+                                    borderRadius: 30 / 2,
+                                    backgroundColor: 'white',
+                                    borderColor: '#b7b7b7',
+                                    borderWidth: 2,
+                                    }
+                                    });
 
 module.exports = HomeScreen;
+
