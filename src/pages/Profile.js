40ee92884e7aad8c0a32e93cf3ca70abeb034bd8
milobeyene <mbeyene@macalester.edu>
2017-11-17 14:38:09 -0600

    Profile page displays current user's data

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -25,28 +25,47 @@ class ProfileScreen extends Component {
         title: 'Profile',
     };
 
-    constructor(props) {
+constructor(props) {
         super(props);
 
         this.state = {
-            email: '',
-            password: '',
-            loaded: true
+            name: '',
+            phone: '',
+            availability: '',
+            status: ''
         }
+    }
+
+    loadData() {
+        let uid = firebaseApp.auth().currentUser.uid;
+        let that = this;
+        firebaseApp.database().ref('Names/' + uid).on("value", function (snapshot) {
+            that.setState({name: snapshot.val()})
+        });
+        firebaseApp.database().ref('PhoneNumbers/' + uid).on("value", function (snapshot) {
+            that.setState({phone: snapshot.val()})
+        });
+    }
+
+    componentDidMount() {
+        this.loadData();
     }
 
     render() {
         const {navigate} = this.props.navigation;
+
         return (
             <View style={styles.container}>
-
                 {/*<Header text="Friends List" loaded={this.state.loaded} />*/}
-
                 <Text style={styles.welcome}>welcome to the profile screen</Text>
+                <Text style={styles.actionText}> Name: {this.state.name}</Text>
+                <Text style={styles.actionText}> Status </Text>
+                <Text style={styles.actionText}> Phone: {this.state.phone} </Text>
+                <Text style={styles.actionText}> About </Text>
+                <Text style={styles.actionText}> Interest </Text>
             </View>
         );
-    }
-
+	}	
 }
 
 
