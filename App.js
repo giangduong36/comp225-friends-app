96ace4de92c0bcce052675baf8b843d98709635d
milobeyene <mbeyene@macalester.edu>
2017-11-22 16:11:15 -0600

    Refactored App.js

--- App.js
+++ App.js
@@ -1,75 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 'use strict';
-
+//Import React
 import React, {Component} from 'react';
-import ReactNative from 'react-native';
-const {
-    AppRegistry,
-    ListView,
-    StyleSheet,
-    Text,
-    View,
-    TouchableHighlight,
-    AlertIOS,
-    TextInput
-} = ReactNative;
-
-// Import components
-const StatusBar = require('./src/components/StatusBar');
-const ActionButton = require('./src/components/ActionButton');
-const ListItem = require('./src/components/ListItem');
-const styles = require('./styles.js');
 
 // Import firebase
 const firebaseApp = require('./src/services/firebaseInit');
 
-// Import pages
-//const MainScreen = require('./src/pages/Main');
-//const SignupScreen = require('./src/pages/Signup');
-//const LoginScreen = require('./src/pages/Login');
-//const AccountScreen = require('./src/pages/Account');
-//const AvailabilityScreen = require('./src/pages/Availability');
-//const FriendslistScreen = require('./src/pages/Friendslist');
-//const SearchFriendScreen = require('./src/pages/SearchFriend');
-//const MatchesScreen = require('./src/pages/Matches');
+//Import Icon
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
+// Import pages
 const InitScreen = require('./src/pages/Init'); //this screen has our app's logo (when a person opens the app for the first time, this is the page they see (look at Trevor's design))
 const SignupScreen = require('./src/pages/Signup'); //this screen is the sign up page
 const LoginScreen = require('./src/pages/Login'); //this screen is the log in page
 const HomeScreen = require('./src/pages/Home'); //this screen is the home screen. on this screen, you can change availabilty, go to choose hangmates, make a status (see trevor's design)
-const ProfileScreen = require('./src/pages/Profile'); //this is the user's profile (you can edit your info here)
-const EditProfileScreen = require('./src/pages/EditProfile');
+const ProfileScreen = require('./src/pages/Profile'); //this is the user's profile (you can view your info here)
+const EditProfileScreen = require('./src/pages/EditProfile'); //this is the user's profile (you can edit your info here)
 const SettingsScreen = require('./src/pages/Settings'); //this is the settings page
 const FriendslistScreen = require('./src/pages/Friendslist'); //this is the list of friends
 const UserDetailScreen = require('./src/pages/UserDetail'); //this screen shows you another user's profile
 const AddFriendScreen = require('./src/pages/AddFriend'); //this is the page where you can search for friends and add them
 const MatchesScreen = require('./src/pages/Matches'); //this is your matches page
 
-//Import Icon
-
-import Icon from 'react-native-vector-icons/MaterialIcons'
-
-// Screen navigation
-
+// Import screen navigation
 import {
     StackNavigator, TabNavigator
 } from 'react-navigation';
-
 import { BottomNavigation, NavigationComponent, Tab } from 'react-native-material-bottom-navigation'
-
-//export const SimpleApp = StackNavigator({
-//    Login: {screen: LoginScreen},
-//    Signup: {screen: SignupScreen},
-//    Main: { screen: MainScreen },
-//    Availability: { screen: AvailabilityScreen },
-//    Account: {screen: AccountScreen},
-//    SearchFriend: {screen: SearchFriendScreen},
-//});
 
 export const InitStack = StackNavigator({
 	Init: {screen: InitScreen},
@@ -80,7 +36,6 @@ export const InitStack = StackNavigator({
 export const FriendStack = StackNavigator({
 	Friendslist: {screen: FriendslistScreen},
 	UserDetail: {screen: UserDetailScreen},
-	//also to add a friend?
 	AddFriend: {screen: AddFriendScreen},
 });
 
@@ -92,12 +47,8 @@ export const MatchesStack = StackNavigator({
 export const HomeStack = StackNavigator({
 	Home: {screen: HomeScreen},
 	Profile: {screen: ProfileScreen},
-	EditProfile: {screen: EditProfileScreen},
-	//also to settings?
-	Settings: {screen: SettingsScreen},
+	EditProfile: {screen: EditProfileScreen}
 });
-
-
 
 export const Tabs = TabNavigator({
   Friends: { screen: FriendStack },
@@ -133,8 +84,6 @@ export const Tabs = TabNavigator({
     }
   }
 })
-
-
 
 export const Root = StackNavigator({
   InitStack: {
